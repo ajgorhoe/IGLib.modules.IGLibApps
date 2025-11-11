63d7e7afb64bba093051417c8636330a2b7c0c30
Igor Grešovnik <gresovnik@gmail.com>
2025-11-11 11:38:18 +0100

    InitModules added

---
+++ src/0InitModules/InitModulesApps/InitModulesAppss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitModulesApps
+{
+
+    /// <summary>Dummy class, just to trigger building the initialization project.
+    /// Access is internal in order not to interfere with names of referencing projects.</summary>
+    internal class InitModulesApps
+    {
+
+    }
+}
