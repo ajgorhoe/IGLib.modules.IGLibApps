f2beaa7d7cd6ccd1646f94dec8dcfd11b35bcf37
Igor Grešovnik <gresovnik@gmail.com>
2026-01-02 12:40:12 +0100

    Updated UpdateDependencyRepos.ps1 - included cloning / updatind of all official IGLib modules

--- scripts/UpdateDependencyRepos.ps1
+++ scripts/UpdateDependencyRepos.ps1
@@ -1,6 +1,6 @@
 
 # Clones or updates the depencencies repositories for IGLibCore repo.
-Write-Host "`n`nCloning / updating basic dependency repositories of IGLibCore repository ...`n"
+Write-Host "`n`nCloning / updating basic dependency repositories of IGLibApps repository ...`n"
 
 # Get the script directory such that relative paths can be resolved:
 $scriptPath = $MyInvocation.MyCommand.Path
@@ -9,8 +9,22 @@ $scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
 
 Write-Host "Script directory: $scriptDir"
 
+Write-Host "`nUpdating IGLib.NET modules:"
+
 Write-Host "`nUpdating IGLibScripts:"
 & $(Join-Path $scriptDir "UpdateRepo_IGLibScripts.ps1")
 
-Write-Host "  ... updating IGLibCore dependencies complete.`n`n"
+Write-Host "`nUpdating IGLibCore:"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibCore.ps1")
+
+Write-Host "`nUpdating IGLibScripting:"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibScripting.ps1")
+
+Write-Host "`nUpdating IGLibScriptingCs:"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibScriptingCs.ps1")
+
+Write-Host "`nUpdating IGLibGraphics3D:"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibGraphics3D.ps1")
+
+Write-Host "  ... updating IGLibApps dependencies complete.`n`n"
 
