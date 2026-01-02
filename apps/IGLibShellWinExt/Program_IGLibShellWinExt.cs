7bb20c58a4d4eb5b4fee1252d840ac1e668468ed
Igor Grešovnik <gresovnik@gmail.com>
2026-01-02 13:29:51 +0100

    Added application - IGLibShellWinExt

---
+++ apps/IGLibShellWinExt/Program_IGLibShellWinExt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace IGLib.App.ShellWin
+{
+    public class ProgramIGLibShellWin
+    {
+
+        public static void Main(string[] args)
+        {
+            AssemblyName assemblyName = Assembly.GetEntryAssembly()!.GetName();
+            string applicationName = assemblyName.Name!;
+            string applicationVerson = assemblyName.Version!.ToString();
+            string invokedAs = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+            Console.WriteLine($"\nApplication {applicationName}, v{applicationVerson}...");
+            Console.WriteLine($"Invoked as: {invokedAs}");
+#if true
+            Console.WriteLine("Command-line arguments:");
+            if (args == null)
+            {
+                Console.WriteLine("  null");
+            } else if (args.Length == 0)
+            {
+                Console.WriteLine("  Empty argument list.");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"  args[{i}] = \"{args[i]}\"");
+                }
+            }
+            Console.WriteLine();
+#endif
+        
+        }
+
+
+    }
+
+
+}
