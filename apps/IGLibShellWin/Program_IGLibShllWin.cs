13cdc40f90be6f2f7dae94b46a3867cab58f9b61
Igor Grešovnik <gresovnik@gmail.com>
2026-01-02 11:51:01 +0100

    Initial implementation - IGLibShellWin app

---
+++ apps/IGLibShellWin/Program_IGLibShllWin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace IGLib.App.ShellWin
+{
+    public class ProgramIGLibShellWin
+    {
+
+        public static void Main(string[] args)
+        {
+            AssemblyName assemblyName = Assembly.GetEntryAssembly()!.GetName();
+            string applicationName = assemblyName.Name!;
+            string applicationVerson = assemblyName.Version!.ToString();
+            Console.WriteLine($"\nApplication {applicationName}, v{applicationVerson}...");
+#if true
+            Console.WriteLine("Command-line arguments:");
+            if (args == null)
+            {
+                Console.WriteLine("  null");
+            } else if (args.Length == 0)
+            {
+                Console.WriteLine("  Empty argument list.");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"  args[{i}] = \"{args[i]}\"");
+                }
+            }
+            Console.WriteLine();
+#endif
+
+
+        }
+
+
+
+    }
+
+
+}
